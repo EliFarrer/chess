b205461f83aba87ca40d6deffa031278c6630dca
Eli Farrer <eli.farrer@gmail.com>
2025-01-23 13:42:58 -0700

    passing most of the tests for rook

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -6,9 +6,34 @@ import java.util.Collection;
 public class RookMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var coll = new ArrayList<ChessMove>();
-
+        for (int i = position.getRow()+1; i < 9; i++) {
+            ChessPosition newPos = new ChessPosition(i, position.getColumn());
+            if (canMove(board, position, newPos)) {
+                coll.add(new ChessMove(position, newPos, null));
+            } else { break; }
+        }
+        for (int i = position.getColumn()+1; i < 9; i++) {
+            ChessPosition newPos = new ChessPosition(position.getRow(), i);
+            if (canMove(board, position, newPos)) {
+                coll.add(new ChessMove(position, newPos, null));
+            } else { break; }
+        }
+        for (int i = position.getRow()-1; i > 0; i--) {
+            ChessPosition newPos = new ChessPosition(i, position.getColumn());
+            if (canMove(board, position, newPos)) {
+                coll.add(new ChessMove(position, newPos, null));
+            } else { break; }
+        }
+        for (int i = position.getColumn()-1; i > 0; i--) {
+            ChessPosition newPos = new ChessPosition(position.getRow(), i);
+            if (canMove(board, position, newPos)) {
+                coll.add(new ChessMove(position, newPos, null));
+            } else { break; }
+        }
         return coll;
     }
+
+
     public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
         return ((board.spotEmpty(newPosition) || board.positionIsNotSameColor(position, newPosition)) && (board.inBounds(newPosition)));
     }
