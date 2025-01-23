857c4bf2417fd3651329ce7802d545290924e7a7
Eli Farrer <eli.farrer@gmail.com>
2025-01-23 13:37:55 -0700

    updated to include a canMove method and completed tests

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -7,9 +7,19 @@ public class KingMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var coll = new ArrayList<ChessMove>();
 
+        int[][] spots = new int[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        for (int[] spot : spots) {
+            ChessPosition newPos = new ChessPosition(position.getRow() + spot[0], position.getColumn() + spot[1]);
+
+            if (canMove(board, position, newPos)) {
+                coll.add(new ChessMove(position, newPos, null));
+            }
+
+        }
         return coll;
     }
-    public boolean inBounds(ChessPosition position) {
-        return (position.getRow() >= 1) & (position.getRow() <= 8) & (position.getColumn() >= 1) & (position.getColumn() <= 8);
+    public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
+        return ((board.spotEmpty(newPosition) || board.positionIsNotSameColor(position, newPosition)) && board.inBounds(newPosition));
     }
+
 }
