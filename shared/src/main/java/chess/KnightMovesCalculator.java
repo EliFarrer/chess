18ecec3400f6483e14bc2d3ccf78878a8c765831
Eli Farrer <eli.farrer@gmail.com>
2025-01-22 18:24:10 -0700

    completed knightmovescalculator with some errors

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -7,6 +7,23 @@ public class KnightMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var coll = new ArrayList<ChessMove>();
 
+        int[][] spots = new int[][]{{-2, -1}, {-2, 1}, {-1, 2}, {1, 2}};
+        for (int[] spot : spots) {
+            ChessPosition newPositionA = new ChessPosition(spot[0] + position.getRow(), spot[1] + position.getColumn());
+            ChessPosition newPositionB = new ChessPosition(spot[1] + position.getRow(), spot[0] + position.getColumn());
+
+            if ((board.getPiece(newPositionA) == null) & inBounds(newPositionA)) {
+                coll.add(new ChessMove(position, newPositionA, null));
+            }
+            if ((board.getPiece(newPositionB) == null) & inBounds(newPositionA)) {
+                coll.add(new ChessMove(position, newPositionB, null));
+            }
+        }
         return coll;
     }
+
+    public boolean inBounds(ChessPosition position) {
+        return (position.getRow() >= 1) & (position.getRow() <= 8) & (position.getColumn() >= 1) & (position.getColumn() <= 8);
+    }
+
 }
