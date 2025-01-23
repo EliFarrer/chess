5ab43c9791560339db1148549f9920cbdcb7ea15
Eli Farrer <eli.farrer@gmail.com>
2025-01-22 17:23:32 -0700

    added KingMovesCalculator

---
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -0,0 +1,12 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KingMovesCalculator implements PieceMovesCalculator{
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        var coll = new ArrayList<ChessMove>();
+
+        return coll;
+    }
+}
