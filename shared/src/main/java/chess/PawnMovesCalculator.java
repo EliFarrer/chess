7017cf0b2da7bbfffb6ada78832152b4a9a7498a
Eli Farrer <eli.farrer@gmail.com>
2025-01-24 09:42:25 -0700

    made canMoveForward method

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -6,11 +6,48 @@ import java.util.Collection;
 public class PawnMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var coll = new ArrayList<ChessMove>();
-
+        // white
+        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            if (position.getRow() == 2) {
+                // first time, two options to move
+            }
+        }
+        // black
+        if (board.getPiece(position).getTeamColor() == ChessGame.TeamColor.BLACK) {
+            if (position.getRow() == 7) {
+                // first time, two options to move
+            }
+        }
+//        addPromotionPieces(coll, new ChessPosition(1,1), new ChessPosition(2,2));
+        //first move has two options
+        //direction is based off of color
+        //promotions
+        //diagonal capturing
+        //shut out
+        //blocked by another piece
         return coll;
     }
 
     public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
         return ((board.spotEmpty(newPosition) || board.positionIsNotSameColor(position, newPosition)) && board.inBounds(newPosition));
     }
+
+    public void addPromotionPieces(ArrayList<ChessMove> array, ChessPosition position, ChessPosition newPosition) {
+        array.add(new ChessMove(position, newPosition, ChessPiece.PieceType.BISHOP));
+        array.add(new ChessMove(position, newPosition, ChessPiece.PieceType.ROOK));
+        array.add(new ChessMove(position, newPosition, ChessPiece.PieceType.QUEEN));
+        array.add(new ChessMove(position, newPosition, ChessPiece.PieceType.KNIGHT));
+    }
+
+    public boolean canMoveForward(ChessBoard board, ChessPosition position) {
+        // pawns can only move forward if the spot ahead of them is empty
+        int newRow;
+        if (board.isPositionWhite(position)) {
+            newRow = position.getRow() + 1;
+        } else {
+            newRow = position.getRow() - 1;
+        }
+        return board.spotEmpty(new ChessPosition(newRow, position.getColumn()));
+    }
+
 }
