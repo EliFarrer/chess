e8f3b918e488fb4f2b66878c801bb852b2fcdc9e
Eli Farrer <eli.farrer@gmail.com>
2025-01-24 09:18:17 -0700

    completed bishop moves

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -6,11 +6,34 @@ import java.util.Collection;
 public class BishopMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         var coll = new ArrayList<ChessMove>();
+        int[][] spots = new int[][] {{1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
 
+        for (int[] spot : spots) {
+            for (int i = 1; i < 9; i++) {
+                ChessPosition newPosition = new ChessPosition((position.getRow() + spot[0] * i), (position.getColumn()) + spot[1] * i);
+                // if we are out of bounds or the new position is a piece of our same color, break immediately
+                if (!board.inBounds(newPosition)) {
+                    break;
+                }
+                if (!board.spotEmpty(newPosition)) {
+                    // if we are attacking
+                    if (board.positionIsNotSameColor(newPosition, position)) {
+                        coll.add(new ChessMove(position, newPosition, null));
+                    }
+                    break;
+
+                }
+                // if no other options, add it
+                else {
+                    coll.add(new ChessMove(position, newPosition, null));
+                }
+            }
+
+        }
         return coll;
     }
 
     public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
-        return ((board.spotEmpty(newPosition) || board.positionIsNotSameColor(position, newPosition)) && board.inBounds(newPosition));
+        return false;
     }
 }
