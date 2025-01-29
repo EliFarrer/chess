b298b8a8324c910ff877f5dcfa8b628860c894d1
Eli Farrer <eli.farrer@gmail.com>
2025-01-29 10:54:41 -0700

    updated kingmoves to implement hopper calculator

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,25 +1,12 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
-public class KingMovesCalculator implements PieceMovesCalculator{
+public class KingMovesCalculator extends HopperPieceCalculator{
+    int[][] spots = new int[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+    @Override
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
-        var coll = new ArrayList<ChessMove>();
-
-        int[][] spots = new int[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        for (int[] spot : spots) {
-            ChessPosition newPos = new ChessPosition(position.getRow() + spot[0], position.getColumn() + spot[1]);
-
-            if (canMove(board, position, newPos)) {
-                coll.add(new ChessMove(position, newPos, null));
-            }
-
-        }
-        return coll;
+        return super.getMoves(spots, board, position);
     }
-    public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
-        return ((board.spotEmpty(newPosition) || board.positionIsNotSameColor(position, newPosition)) && board.inBounds(newPosition));
-    }
-
 }
