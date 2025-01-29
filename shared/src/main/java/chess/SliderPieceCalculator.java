0d7b9e29c4c8b2c4a81d44503d34588f6e102852
Eli Farrer <eli.farrer@gmail.com>
2025-01-29 10:21:31 -0700

    added abstract class for sliding pieces (queen, king, rook)

---
+++ shared/src/main/java/chess/SliderPieceCalculator.java
@@ -0,0 +1,41 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class SliderPieceCalculator implements PieceMovesCalculator {
+    private final int[][] spots;
+
+    SliderPieceCalculator(int[][] spots) {
+        this.spots = spots;
+    }
+
+    public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
+
+        var coll = new ArrayList<ChessMove>();
+
+        for (int[] spot : spots) {
+            for (int i = 1; i < 9; i++) {
+                ChessPosition newPosition = new ChessPosition((position.getRow() + spot[0] * i), (position.getColumn()) + spot[1] * i);
+                // if we are out of bounds or the new position is a piece of our same color, break immediately
+                if (!board.inBounds(newPosition)) {
+                    break;
+                }
+                if (!board.spotEmpty(newPosition)) {
+                    // if we are attacking
+                    if (board.positionIsNotSameColor(newPosition, position)) {
+                        coll.add(new ChessMove(position, newPosition, null));
+                    }
+                    break;
+
+                }
+                // if no other options, add it
+                else {
+                    coll.add(new ChessMove(position, newPosition, null));
+                }
+            }
+
+        }
+        return coll;
+    }
+}
