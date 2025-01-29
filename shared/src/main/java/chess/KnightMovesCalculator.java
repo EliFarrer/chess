51f18905c8227811179744e8484a1a0a3536cdd2
Eli Farrer <eli.farrer@gmail.com>
2025-01-29 10:54:33 -0700

    updated knightmoves to implement hopper calculator

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,27 +1,12 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
-public class KnightMovesCalculator implements PieceMovesCalculator{
+public class KnightMovesCalculator extends HopperPieceCalculator{
+    int[][] spots = new int[][]{{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
+
+    @Override
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
-        var coll = new ArrayList<ChessMove>();
-
-        int[][] spots = new int[][]{{-2, -1}, {-2, 1}, {-1, 2}, {1, 2}};
-        for (int[] spot : spots) {
-            ChessPosition newPositionA = new ChessPosition(spot[0] + position.getRow(), spot[1] + position.getColumn());
-            ChessPosition newPositionB = new ChessPosition(spot[1] + position.getRow(), spot[0] + position.getColumn());
-
-            if (canMove(board, position, newPositionA)) {
-                coll.add(new ChessMove(position, newPositionA, null));
-            }
-            if (canMove(board, position, newPositionB)) {
-                coll.add(new ChessMove(position, newPositionB, null));
-            }
-        }
-        return coll;
-    }
-    public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
-        return ((board.spotEmpty(newPosition) || board.positionIsNotSameColor(position, newPosition)) && board.inBounds(newPosition));
+        return super.getMoves(spots, board, position);
     }
 }
