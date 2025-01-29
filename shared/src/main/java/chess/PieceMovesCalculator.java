3978fcf32159a61906465aefe3317fd2b7ce1cc1
Eli Farrer <eli.farrer@gmail.com>
2025-01-29 10:54:11 -0700

    removed canMove form move calculator interface

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -5,8 +5,6 @@ import java.util.Collection;
 public interface PieceMovesCalculator {
     Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position);
 
-//    public boolean inBounds(ChessPosition position);
-
-//    public boolean spotEmpty(ChessBoard board, ChessPosition position);
-boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition);
+//    boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition);
 }
+
