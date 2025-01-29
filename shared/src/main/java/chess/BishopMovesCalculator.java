306b5fa2d7b70e87a0cfaf2c3da5f72544329c53
Eli Farrer <eli.farrer@gmail.com>
2025-01-29 10:41:04 -0700

    updated BishopMovesCalculator to extend Slider

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -3,34 +3,12 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class BishopMovesCalculator implements PieceMovesCalculator{
+public class BishopMovesCalculator extends SliderPieceCalculator{
+    private final int[][] spots = new int[][] {{1, 1}, {-1, 1}, {-1, -1}, {1, -1}}; // just the bishop and rook combined
+
+
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
-        var coll = new ArrayList<ChessMove>();
-        int[][] spots = new int[][] {{1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
-
-        for (int[] spot : spots) {
-            for (int i = 1; i < 9; i++) {
-                ChessPosition newPosition = new ChessPosition((position.getRow() + spot[0] * i), (position.getColumn()) + spot[1] * i);
-                // if we are out of bounds or the new position is a piece of our same color, break immediately
-                if (!board.inBounds(newPosition)) {
-                    break;
-                }
-                if (!board.spotEmpty(newPosition)) {
-                    // if we are attacking
-                    if (board.positionIsNotSameColor(newPosition, position)) {
-                        coll.add(new ChessMove(position, newPosition, null));
-                    }
-                    break;
-
-                }
-                // if no other options, add it
-                else {
-                    coll.add(new ChessMove(position, newPosition, null));
-                }
-            }
-
-        }
-        return coll;
+        return super.getMoves(spots, board, position);
     }
 
     public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
