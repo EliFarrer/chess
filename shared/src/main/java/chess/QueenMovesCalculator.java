ab77e15cd9e3586131c8720c6019752aa77fa4bd
Eli Farrer <eli.farrer@gmail.com>
2025-01-29 10:30:17 -0700

    updated queenmoves to inherit form SliderPiece

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -3,35 +3,14 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class QueenMovesCalculator implements PieceMovesCalculator{
+public class QueenMovesCalculator extends SliderPieceCalculator {
+    private final int[][] spots = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}}; // just the bishop and rook combined
+
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
-        var coll = new ArrayList<ChessMove>();
-        int[][] spots = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}}; // just the bishop and rook combined
-
-        for (int[] spot : spots) {
-            for (int i = 1; i < 9; i++) {
-                ChessPosition newPosition = new ChessPosition((position.getRow() + spot[0] * i), (position.getColumn()) + spot[1] * i);
-                // if we are out of bounds or the new position is a piece of our same color, break immediately
-                if (!board.inBounds(newPosition)) {
-                    break;
-                }
-                if (!board.spotEmpty(newPosition)) {
-                    // if we are attacking
-                    if (board.positionIsNotSameColor(newPosition, position)) {
-                        coll.add(new ChessMove(position, newPosition, null));
-                    }
-                    break;
-
-                }
-                // if no other options, add it
-                else {
-                    coll.add(new ChessMove(position, newPosition, null));
-                }
-            }
-
-        }
-        return coll;
+        super.setSpots(spots);
+        return super.getMoves(board, position);
     }
+
     public boolean canMove(ChessBoard board, ChessPosition position, ChessPosition newPosition) {
         return false;
     }
