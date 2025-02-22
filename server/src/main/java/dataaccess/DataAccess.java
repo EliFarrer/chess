ded1d0b40f7ea156339cb73472b1c13e4ceb7bcc
Eli Farrer <eli.farrer@gmail.com>
2025-02-22 13:24:36 -0700

    added Data Access interface

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,6 @@
+package dataaccess;
+
+public interface DataAccess {
+    // in here are all the declarations of the methods we need to run eventually like clear and add and stuff like that
+    void clearAllEntries();
+}
