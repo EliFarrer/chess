bba8531b9c9b797fda4ed92eac3347d03671cbac
Eli Farrer <eli.farrer@gmail.com>
2025-02-22 14:27:17 -0700

    added test file

---
+++ server/src/test/java/service/ServiceTests.java
@@ -0,0 +1,64 @@
+package service;
+import org.junit.jupiter.api.Test;
+import service.Service;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServiceTests {
+    @Test
+    public void testClear_Positive() {
+
+    }
+    @Test
+    public void testClear_Negative() {
+
+    }
+    @Test
+    public void testRegister_Positive() {
+
+    }
+    @Test
+    public void testRegister_Negative() {
+
+    }
+    @Test
+    public void testLogin_Positive() {
+
+    }
+    @Test
+    public void testLogin_Negative() {
+
+    }
+    @Test
+    public void testLogout_Positive() {
+
+    }
+    @Test
+    public void testLogout_Negative() {
+
+    }
+    @Test
+    public void testCreate_Positive() {
+
+    }
+    @Test
+    public void testCreate_Negative() {
+
+    }
+    @Test
+    public void testJoin_Positive() {
+
+    }
+    @Test
+    public void testJoin_Negative() {
+
+    }
+    @Test
+    public void testList_Positive() {
+
+    }
+    @Test
+    public void testList_Negative() {
+
+    }
+
+}
