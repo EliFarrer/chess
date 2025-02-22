162c61c4c9a90fe29c71a2bfe8c8113a73f9a820
Eli Farrer <eli.farrer@gmail.com>
2025-02-22 13:19:58 -0700

    server runs

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,23 @@
-import chess.*;
+import server.Server;
+import service.Service;
+import dataaccess.DAO;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+
+            DataAccess
+
+            Service service = new Service();
+            Server server = new Server(service);
+            int port = Integer.parseInt(args[0]);
+            server.run(port);
+
+        } catch(ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            System.err.println("Put in an integer for the port you want to use.");
+        }
+
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
     }
 }
