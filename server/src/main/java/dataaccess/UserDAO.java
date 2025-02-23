635bc553a661c4519cfaae4df9a92e716e990c4d
Eli Farrer <eli.farrer@gmail.com>
2025-02-22 17:00:11 -0700

    renamed user DAO

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,6 +1,13 @@
 package dataaccess;
 
-public interface DataAccess {
+import model.UserData;
+
+public interface UserDAO {
     // in here are all the declarations of the methods we need to run eventually like clear and add and stuff like that
     void clearAllEntries();
+
+    void createUser(UserData userData);
+
+    UserData getUser(String username);
+
 }
