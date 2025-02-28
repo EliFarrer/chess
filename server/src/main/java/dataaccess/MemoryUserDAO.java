39f44f6dd274d3b9a5b464e097853a8ec8fb36f3
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 11:53:53 -0700

    added exception throwing

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,21 +2,21 @@ package dataaccess;
 
 import model.UserData;
 
+import javax.xml.crypto.Data;
 import java.util.Map;
 
 public class MemoryUserDAO implements UserDAO {
     public Map<String, UserData> data;
 
-    public void clearAllEntries() {
+    public void clearAllEntries() throws DataAccessException {
         data.clear();
     }
 
-    public void createUser(UserData userData) {
+    public void createUser(UserData userData) throws DataAccessException {
         data.put(userData.username(), userData);
     }
 
-    public UserData getUser(String username) {
+    public UserData getUser(String username) throws DataAccessException {
         return data.get(username);
     }
-
 }
