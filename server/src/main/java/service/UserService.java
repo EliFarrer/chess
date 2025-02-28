7f638126b90bae9ba64e6585cbeb22aa71911a40
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 11:53:24 -0700

    added basic classes for a UserService

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,9 @@
 package service;
 
 public class UserService {
+    public RegisterResult register(RegisterRequest req) {
+        return new RegisterResult("", "");
+    }
+    public void login() {}
+    public void logout() {}
 }
