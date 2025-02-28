14b7d8ca209f17986988fe84e035bef60829d082
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 12:59:10 -0700

    completed register tests

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -1,23 +1,43 @@
 package service;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import request.RegisterRequest;
+import result.RegisterResult;
+
+import java.util.Objects;
 
 public class ServiceTests {
     @Test
     public void testClear_Positive() {
-
+//        MemoryUserDAO dao = new MemoryUserDAO();
+//        UserData data = new UserData("eli", "ile", "eli@ile.com");
+//        dao.createUser(data);
+        ClearService service = new ClearService(null);
+        ClearResult rtrn = service.clearGame();
+        assert(rtrn.message().isEmpty());
     }
-    @Test
-    public void testClear_Negative() {
 
-    }
+    // don't need a negative testClear test case
+
     @Test
     public void testRegister_Positive() {
-
+        UserService service = new UserService();
+        RegisterRequest req = new RegisterRequest("eli", "ile", "eli@ile.com");
+        RegisterResult res = service.register(req);
+        Assertions.assertEquals(req.userName(), res.username());
+        Assertions.assertNotNull(res.authToken());
     }
     @Test
-    public void testRegister_Negative() {
-
+    public void testRegister_Negative_userAlreadyRegistered() {
+        UserService service = new UserService();
+        RegisterRequest req = new RegisterRequest("eli", "ile", "eli@ile.com");
+        service.register(req);
+        Exception ex = Assertions.assertThrows(ServiceException.class, () -> service.register(req));
+        Assertions.assertEquals("the user is already registered", ex.getMessage());
     }
+
+//    public void testRegister_Negative_badData() {}
+
     @Test
     public void testLogin_Positive() {
 
