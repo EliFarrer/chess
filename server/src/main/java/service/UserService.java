d455c9df450df53bd114a2c70266560bc7c64c7f
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 16:23:26 -0700

    added login

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -5,41 +5,59 @@ import dataaccess.MemoryUserDAO;
 import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
+import request.LoginRequest;
 import request.RegisterRequest;
-import result.RegisterResult;
+import result.LoginResult;
+
+import java.util.Objects;
 
 public class UserService {
     private final UserDAO dataAccess;
 
-    public UserService() {
-        this.dataAccess = new MemoryUserDAO();
+    public UserService(UserDAO dao) {
+        this.dataAccess = Objects.requireNonNullElseGet(dao, () -> new MemoryUserDAO(null, null));
     }
 
-    public RegisterResult register(RegisterRequest req) throws ServiceException {
+    public LoginResult register(RegisterRequest req) throws ServiceException {
         try {
             // check to see if the user is already registered
-            UserData testUserData = dataAccess.getUser(req.userName());
+            UserData testUserData = dataAccess.getUser(req.username());
             if (testUserData != null) {
                 throw new ServiceException("the user is already registered");
             }
 
             // 400 error, the data is bad
-            if (req.userName().isEmpty() || req.password().isEmpty() || req.email().isEmpty()) {
+            if (req.username().isEmpty() || req.password().isEmpty() || req.email().isEmpty()) {
                 throw new ServiceException("bad data");
             }
 
             // create the new user
-            UserData userData = new UserData(req.userName(), req.password(), req.email());
+            UserData userData = new UserData(req.username(), req.password(), req.email());
             dataAccess.createUser(userData);
             // get the authentication
-            AuthData authData = dataAccess.createAuth(userData.userName());
+            AuthData authData = dataAccess.createAuth(userData.username());
             // return the username and authToken.
-            return new RegisterResult(authData.username(), authData.authToken());
+            return new LoginResult(authData.username(), authData.authToken());
         } catch (DataAccessException e) {
             // this is the case where the database is broken.
             throw new ServiceException("Data access exception: " + e);
         }
     }
-    public void login() {}
+    public LoginResult login(LoginRequest req) {
+        try {
+            UserData userData = dataAccess.getUser(req.username());
+
+            // if the user is not registered
+            if (userData == null) {
+                throw new ServiceException("this user is not registered");
+            }
+
+            // the user is registered, so create auth and return LoginResult
+            AuthData authData = dataAccess.createAuth(userData.username());
+            return new LoginResult(authData.username(), authData.authToken());
+        } catch (DataAccessException e) {
+            throw new ServiceException("Data access exception: " + e);
+        }
+    }
     public void logout() {}
 }
