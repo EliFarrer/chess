d2abae6571f0ab9db034d2410a6d2846d5092c52
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 12:33:20 -0700

    added createAuth

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,12 +1,15 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
 public interface UserDAO {
     // in here are all the declarations of the methods we need to run eventually like clear and add and stuff like that
     void clearAllEntries() throws DataAccessException;
 
-    void createUser(UserData userData) throws DataAccessException ;
+    void createUser(UserData userData) throws DataAccessException;
 
-    UserData getUser(String username) throws DataAccessException ;
+    UserData getUser(String username) throws DataAccessException;
+
+    AuthData createAuth(String userName);
 }
