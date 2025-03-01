ebf3c33769dfa088a4a3d9a025800cf90ffce378
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 19:17:19 -0700

    added test functionality

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,11 +1,12 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
 import dataaccess.MemoryUserDAO;
 import dataaccess.UserDAO;
+import model.GameData;
 import request.CreateGameRequest;
-import result.JoinGameResult;
-import result.ListGamesResult;
+import request.JoinGameRequest;
 
 import java.util.Objects;
 
@@ -22,16 +23,37 @@ public class GameService {
             if (req.authToken().isEmpty() || req.gameName().isEmpty()) {
                 throw new ServiceException("bad data");
             }
-            if (!dataAccess.isAuthorized(req.authToken())) {
+            if (dataAccess.isNotAuthorized(req.authToken())) {
                 throw new ServiceException("unauthorized auth data");
             }
             int gameID = req.gameName().hashCode();
             dataAccess.createGame(gameID, req.gameName());
             return gameID;
         } catch(DataAccessException e) {
-            throw new ServiceException("Data access exception: " + e);
+            throw new ServiceException("Data access exception (creating game): " + e);
         }
     }
 
-//    public JoinGameResult joinGame(String authToken) {}
+    public void joinGame(JoinGameRequest req) {
+        try {
+            if (req.gameID().isEmpty() || req.playerColor().isEmpty()) {
+                throw new ServiceException("bad data");
+            }
+            if (dataAccess.isNotAuthorized(req.authToken())) {
+                throw new ServiceException("unauthorized auth data");
+            }
+//            if (dataAccess.gameNotAuthorized(req.gameID())) {
+//                throw new ServiceException("unauthorized game id");
+//            }
+//            GameData gameData = dataAccess.getGame(req.gameID());
+//            ChessGame.TeamColor requestedColor = req.playerColor().equals("WHITE") ? ChessGame.TeamColor.WHITE : ChessGame.TeamColor.BLACK;
+//            if (dataAccess.colorNotAvailable(requestedColor)) {
+//                throw new ServiceException("color already taken");
+//            }
+
+
+        } catch(DataAccessException e) {
+            throw new ServiceException("Data access exception (joining game): " + e);
+        }
+    }
 }
