ffc4f6f9445036a8c887b98f0ee030d2dae1b07d
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 18:45:46 -0700

    updated for create game

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,37 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import request.CreateGameRequest;
+import result.JoinGameResult;
+import result.ListGamesResult;
+
+import java.util.Objects;
+
 public class GameService {
+    private final UserDAO dataAccess;
+    public GameService(MemoryUserDAO dao) {
+        this.dataAccess = Objects.requireNonNullElseGet(dao, () -> new MemoryUserDAO(null, null, null));
+    }
+
+//    public ListGamesResult listGames(String authToken) {}
+
+    public Integer createGame(CreateGameRequest req) {
+        try {
+            if (req.authToken().isEmpty() || req.gameName().isEmpty()) {
+                throw new ServiceException("bad data");
+            }
+            if (!dataAccess.isAuthorized(req.authToken())) {
+                throw new ServiceException("unauthorized auth data");
+            }
+            int gameID = req.gameName().hashCode();
+            dataAccess.createGame(gameID, req.gameName());
+            return gameID;
+        } catch(DataAccessException e) {
+            throw new ServiceException("Data access exception: " + e);
+        }
+    }
+
+//    public JoinGameResult joinGame(String authToken) {}
 }
