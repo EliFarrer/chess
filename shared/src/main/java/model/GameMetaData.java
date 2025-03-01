8eeae394aa01b7e2f9da68b9de137e6a96dd5457
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 20:31:16 -0700

    added alternate constructor

--- shared/src/main/java/model/GameMetaData.java
+++ shared/src/main/java/model/GameMetaData.java
@@ -4,4 +4,7 @@ import chess.ChessGame;
 
 // same as GameData, but doesn't contain the actual game
 public record GameMetaData(int gameId, String whiteUsername, String blackUsername, String gameName) {
+    public GameMetaData(GameData gameData) {
+        this(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName());
+    }
 }
