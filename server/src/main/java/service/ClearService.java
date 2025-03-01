e06d8a4edba814bfb7f2c1cc88892ddf6bfa267e
Eli Farrer <eli.farrer@gmail.com>
2025-02-28 18:47:54 -0700

    added try block

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,15 +1,25 @@
 package service;
+import dataaccess.DataAccessException;
 import dataaccess.MemoryUserDAO;
 import dataaccess.UserDAO;
+
+import java.util.Objects;
 
 public class ClearService {
     private final UserDAO dataAccess;
     public ClearService() {
-        this.dataAccess = new MemoryUserDAO();
+        // if we pass in a dao, it will use that, if not it will use a newly created DAO.
+        this.dataAccess = new MemoryUserDAO(null, null, null);
     }
 
     public void clearGame() {
-        dataAccess.clearAllEntries();
+        try {
+            dataAccess.clear();
+//            return new ClearResult("");
+        } catch (DataAccessException ex) {
+//            return new ClearResult("did not clear");
+        }
+
     }
 
 }
