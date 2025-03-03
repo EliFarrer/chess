11b1eea4a70bfc9c14288a833c17ca84dcd1f447
Eli Farrer <eli.farrer@gmail.com>
2025-03-03 11:48:00 -0700

    got clear handler working

--- server/src/main/java/HTTPHandler/ClearHandler.java
+++ server/src/main/java/HTTPHandler/ClearHandler.java
@@ -1,13 +1,31 @@
 package HTTPHandler;
+import com.google.gson.Gson;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import service.ClearService;
+import service.ServiceException;
 import spark.*;
+
+import java.util.Map;
 
 
 public class ClearHandler implements Route {
-    public Object handle(Request req, Response res) {
+    ClearService service;
+    Gson serializer;
+    public ClearHandler(MemoryUserDAO dao) {
+        service = new ClearService(dao);
+        serializer = new Gson();
+    }
 
-//        service.clearGame();
-//        res.status(200);
-        return "";
+    public Object handle(Request req, Response res) {
+        try {
+            service.clearGame();
+            return "";
+        } catch (ServiceException e) {
+            res.status(500);
+            return serializer.toJson(e.getMessage());
+        }
+
     }
 
 }
