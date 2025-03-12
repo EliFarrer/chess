d4ee689275cc3fceabef17a1e17096cbb86d328b
Eli Farrer <eli.farrer@gmail.com>
2025-03-12 13:23:27 -0600

    changed minor syntax, added getGame and updateGame

--- server/src/main/java/dataaccess/DatabaseDAO.java
+++ server/src/main/java/dataaccess/DatabaseDAO.java
@@ -12,11 +12,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.mindrot.jbcrypt.BCrypt;
 
-import javax.xml.crypto.Data;
 
 public class DatabaseDAO implements DataAccess {
     String[] databases = {"user", "game", "auth"};
@@ -53,7 +53,7 @@ public class DatabaseDAO implements DataAccess {
         DatabaseManager.createTables();
         UserData secureUserData = new UserData(userData.username(), BCrypt.hashpw(userData.password(), BCrypt.gensalt()), userData.email());
         try (var conn = DatabaseManager.getConnection()) {
-            try (var statement = conn.prepareStatement("insert into user (username, userData) values (?, ?)")) {
+            try (var statement = conn.prepareStatement("INSERT INTO user (username, userData) VALUES (?, ?)")) {
                 statement.setString(1, secureUserData.username());
                 // make a UserData class and convert it to json
                 var json = new Gson().toJson(secureUserData);
@@ -75,8 +75,7 @@ public class DatabaseDAO implements DataAccess {
                 try (var rs = statement.executeQuery()) {
                     rs.next();
                     var json = rs.getString("userData");
-                    var builder = new GsonBuilder();
-                    Gson gson = builder.create();
+                    var gson = new GsonBuilder().create();
                     return gson.fromJson(json, UserData.class);
                 }
             }
@@ -108,7 +107,7 @@ public class DatabaseDAO implements DataAccess {
         DatabaseManager.createTables();
         String id = UUID.randomUUID().toString();
         try (var conn = DatabaseManager.getConnection()) {
-            try (var statement = conn.prepareStatement("insert into auth (id, username) values (?, ?)")) {
+            try (var statement = conn.prepareStatement("INSERT INTO auth (id, username) VALUES (?, ?)")) {
                 statement.setString(1, id);
                 statement.setString(2, username);
                 statement.executeUpdate();
@@ -181,32 +180,101 @@ public class DatabaseDAO implements DataAccess {
      */
     public void createGame(int gameID, String gameName) throws DataAccessException {
         DatabaseManager.createTables();
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("INSERT INTO game (gameID, gameData) VALUES (?, ?)")) {
+                statement.setInt(1, gameID);
 
-        //
+                GameData gameData = new GameData(gameID, "", "", gameName, new ChessGame());
+                var json = new Gson().toJson(gameData);
+                statement.setString(2, json);
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
         DatabaseManager.createTables();
-
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            String select = "SELECT gameData FROM game WHERE gameID = ?";
+            try (var statement = conn.prepareStatement(select)) {
+                statement.setInt(1, gameID);
+                try (var rs = statement.executeQuery()) {
+                    if (!rs.next()) {   // if false, return null
+                        return null;
+                    }
+                    // if not, parse
+                    var json = rs.getString("gameData");
+                    var gson = new GsonBuilder().create();
+                    return gson.fromJson(json, GameData.class);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean gameNotAuthorized(int gameID) throws DataAccessException {
         DatabaseManager.createTables();
-
-        return false;
-    }
-
-    public boolean colorNotAvailable(int gameID, ChessGame.TeamColor requestedColor) throws DataAccessException {
-        DatabaseManager.createTables();
-
-        return false;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("SELECT gameID FROM game WHERE gameID = ?")) {
+                statement.setInt(1, gameID);
+                try (var rs = statement.executeQuery()) {
+                    // if there are not any usernames that match the auth, then rs.next() will return false. So we return true
+                    return !rs.next();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     public void updateGame(int gameID, GameData gameData) throws DataAccessException {
         DatabaseManager.createTables();
+        try (var connection = DatabaseManager.getConnection()) {
+            try (PreparedStatement stmt = connection.prepareStatement("DELETE FROM game WHERE gameID = ?")) {
+                stmt.setInt(1, gameID);
+                stmt.executeUpdate();
+            }
+            try (var statement = connection.prepareStatement("INSERT INTO game (gameID, gameData) VALUES (?, ?)")) {
+                statement.setInt(1, gameID);
 
+                var json = new Gson().toJson(gameData);
+                statement.setString(2, json);
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public boolean colorNotAvailable(int gameID, ChessGame.TeamColor requestedColor) throws DataAccessException {
+        DatabaseManager.createTables();
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("SELECT gameData FROM game WHERE gameID = ?")) {
+                statement.setInt(1, gameID);
+                try (var rs = statement.executeQuery()) {
+                    // check to make sure our data exists
+                    if (!rs.next()) {
+                        throw new DataAccessException("Error: game data doesn't exist");
+                    }
+                    // parse our game data
+                    var json = rs.getString("gameData");
+                    var gson = new GsonBuilder().create();
+                    GameData gameData = gson.fromJson(json, GameData.class);
+
+                    // check to see if the color is available
+                    if (requestedColor == ChessGame.TeamColor.WHITE) {
+                        return !Objects.equals(gameData.whiteUsername(), "");
+                    } else {
+                        return !Objects.equals(gameData.blackUsername(), "");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     public ArrayList<GameMetaData> listGames() throws DataAccessException {
@@ -218,7 +286,7 @@ public class DatabaseDAO implements DataAccess {
     public boolean isGameDatabaseEmpty() throws DataAccessException {
         DatabaseManager.createTables();
         try (var connection = DatabaseManager.getConnection()) {
-            try (PreparedStatement stmt = connection.prepareStatement("SELECT username FROM game")) {
+            try (PreparedStatement stmt = connection.prepareStatement("SELECT gameID FROM game")) {
                 ResultSet rs = stmt.executeQuery();
                 if (!rs.next()) {   // if we are past the last row, then it is empty
                     return true;
