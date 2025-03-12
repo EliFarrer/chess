529074bce62d454c43f441d99e43f08aaf889bd4
Eli Farrer <eli.farrer@gmail.com>
2025-03-11 18:18:18 -0600

    renamed

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -7,7 +7,7 @@ import model.GameMetaData;
 import model.UserData;
 import java.util.ArrayList;
 
-public interface UserDAO {
+public interface DataAccess {
     // in here are all the declarations of the methods we need to run eventually like clear and add and stuff like that
     void clear() throws DataAccessException;
 
