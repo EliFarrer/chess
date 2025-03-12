5053aac91ea9c9ccf011d9ab43f6d28e7913cc84
Eli Farrer <eli.farrer@gmail.com>
2025-03-12 10:23:39 -0600

    added reset method

--- server/src/test/java/dataaccess/ClearDatabaseTests.java
+++ server/src/test/java/dataaccess/ClearDatabaseTests.java
@@ -1,16 +1,9 @@
 package dataaccess;
 
-import chess.ChessGame;
-import model.AuthData;
-import model.GameData;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import service.ClearService;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.HashMap;
 
 public class ClearDatabaseTests {
     @BeforeEach
@@ -44,4 +37,15 @@ public class ClearDatabaseTests {
 
     }
     // don't need a negative testClear test case
+    @AfterAll
+    public static void reset() {
+        try {
+            // clear the tables
+            DatabaseManager.dropTables();
+            // add the tables
+            DatabaseManager.createTables();
+        } catch (DataAccessException e) {
+            throw new RuntimeException("Error: Creating or deleting tables failed.");
+        }
+    }
 }
