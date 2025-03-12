90e51428bd95a1b73996855dcda7a60e0ca76c77
Eli Farrer <eli.farrer@gmail.com>
2025-03-12 13:23:50 -0600

    added tests for gameNotAuthorized and colorNOtAvailable

--- server/src/test/java/dataaccess/GameDatabaseTests.java
+++ server/src/test/java/dataaccess/GameDatabaseTests.java
@@ -34,22 +34,28 @@ public class GameDatabaseTests {
 
     @Test
     public void testCreateGamePositive() {
-//        try (var conn = DatabaseManager.getConnection()) {
-//            try (var stmt = conn.prepareStatement("insert into game (gameID, gameData) values (")) {
-//
-//            }
-//        } catch (DataAccessException e) {
-//            throw new RuntimeException("Error: Adding auth data to database failed");
-//        }
-        // here I will need to do the translation thing with json....
+        DatabaseDAO db = new DatabaseDAO();
+        int id = 1;
+        Assertions.assertDoesNotThrow(() -> {
+            Assertions.assertTrue(db.isGameDatabaseEmpty());
+            db.createGame(id, "myChessGame");
+            Assertions.assertFalse(db.isGameDatabaseEmpty());
+        });
     }
+
     @Test
     public void testCreateGameNegative() {
 
     }
     @Test
     public void testGetGamePositive() {
-
+        DatabaseDAO db = new DatabaseDAO();
+        int id = 1;
+        Assertions.assertDoesNotThrow(() -> {
+            Assertions.assertNull(db.getGame(id));
+            db.createGame(id, "myChessGame");
+            Assertions.assertNotNull(db.getGame(id));
+        });
     }
     @Test
     public void testGetGameNegative() {
@@ -57,30 +63,58 @@ public class GameDatabaseTests {
     }
     @Test
     public void testGameNotAuthorizedPositive() {
-
+        DatabaseDAO db = new DatabaseDAO();
+        int id = 1;
+        Assertions.assertDoesNotThrow(() -> {
+            Assertions.assertTrue(db.gameNotAuthorized(id));
+            db.createGame(id, "myChessGame");
+            Assertions.assertFalse(db.gameNotAuthorized(id));
+        });
     }
+
     @Test
     public void testGameNotAuthorizedNegative() {
 
     }
-    @Test
-    public void testColorNotAvailablePositive() {
 
-    }
-    @Test
-    public void testColorNotAvailableNegative() {
-
-    }
     @Test
     public void testUpdateGamePositive() {
-
+        DatabaseDAO db = new DatabaseDAO();
+        int id = 1;
+        String gameName = "myChessGame";
+        Assertions.assertDoesNotThrow(() -> {
+            db.createGame(id, gameName);
+            GameData expectedGameData = new GameData(id, "ile", "eli", gameName, new ChessGame());
+            db.updateGame(id, expectedGameData);
+            Assertions.assertEquals(expectedGameData, db.getGame(id));
+        });
     }
     @Test
     public void testUpdateGameNegative() {
 
     }
+
+    @Test
+    public void testColorNotAvailablePositive() {
+        DatabaseDAO db = new DatabaseDAO();
+        int id = 1;
+        String gameName = "myChessGame";
+        Assertions.assertDoesNotThrow(() -> {
+            db.createGame(id, gameName);
+            db.updateGame(id, new GameData(id, "", "eli", gameName, new ChessGame()));
+            Assertions.assertFalse(db.colorNotAvailable(id, ChessGame.TeamColor.WHITE));
+            Assertions.assertTrue(db.colorNotAvailable(id, ChessGame.TeamColor.BLACK));
+        });
+    }
+
+    @Test
+    public void testColorNotAvailableNegative() {
+
+    }
+
     @Test
     public void testListGamesPositive() {
+
 
     }
     @Test
