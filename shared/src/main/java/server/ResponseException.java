b7d5e98b2f98a2a8be177203fa10e001105d5bbf
Eli Farrer <eli.farrer@gmail.com>
2025-03-25 19:29:37 -0600

    made it so we don't have to get the status code from the body

--- shared/src/main/java/server/ResponseException.java
+++ shared/src/main/java/server/ResponseException.java
@@ -29,4 +29,10 @@ public class ResponseException extends RuntimeException {
     String message = map.get("message").toString();       // get the message
     return new ResponseException(status, message);        // return the exception
   }
+
+  public static ResponseException fromJson(int statusCode, InputStream stream) {
+    var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);  // create a map of the string
+    String message = map.get("message").toString();       // get the message
+    return new ResponseException(statusCode, message);        // return the exception
+  }
 }
