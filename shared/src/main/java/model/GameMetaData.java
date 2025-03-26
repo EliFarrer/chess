a28c153855a844c3f6c71cbe8826ba8ec93e0c6e
Eli Farrer <eli.farrer@gmail.com>
2025-03-25 20:34:50 -0600

    added toString method

--- shared/src/main/java/model/GameMetaData.java
+++ shared/src/main/java/model/GameMetaData.java
@@ -5,4 +5,14 @@ public record GameMetaData(int gameID, String whiteUsername, String blackUsernam
     public GameMetaData(GameData gameData) {
         this(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName());
     }
+
+    @Override
+    public String toString() {
+        String white = whiteUsername == null ? "?" : whiteUsername;
+        String black = blackUsername == null ? "?" : blackUsername;
+        return "Game " + gameName +
+                ", white=" + white +
+                ", black=" + black  +
+                ", id=" + gameID;
+    }
 }
