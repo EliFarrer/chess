4fd719fd2069d5c132cecaa5185cab937a93ae44
Eli Farrer <eli.farrer@gmail.com>
2025-04-07 16:02:19 -0600

    added basics of web socket facade

---
+++ client/src/main/java/ui/websocket/WebSocketFacade.java
@@ -0,0 +1,41 @@
+package ui.websocket;
+import com.google.gson.Gson;
+import server.ResponseException;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class WebSocketFacade extends Endpoint {
+    public Session session;
+    public ServerMessageHandler messageHandler;
+
+    public WebSocketFacade(int port, ServerMessageHandler messageHandler) throws ResponseException {
+        try {
+            String url = "ws://localhost:" + port;
+            URI socketURI = new URI(url + "/ws");
+            this.messageHandler = messageHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    messageHandler.notify(notification);
+
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {}
+
+}
