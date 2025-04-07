4b4b9a7f7ffd81a5ea51c0588c290c1fb0df2f52
Eli Farrer <eli.farrer@gmail.com>
2025-04-07 16:36:37 -0600

    added ws and authToken

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,10 +1,18 @@
 package ui;
 
+import server.ResponseException;
+import ui.client.GameplayClient;
+import ui.client.PostLoginClient;
+import ui.client.PreLoginClient;
+import ui.websocket.ServerMessageHandler;
+import ui.websocket.WebSocketFacade;
+import websocket.messages.ServerMessage;
+
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
 
-public class Repl {
+public class Repl implements ServerMessageHandler {
     State state = State.PRE_LOGIN;
     final int port;
     PreLoginClient preLogin;
@@ -12,13 +20,16 @@ public class Repl {
     GameplayClient gameplay;
     ServerFacade server;
     Integer currentGameID = null;
+    String authToken;
+    WebSocketFacade ws;
 
     public Repl(int port) {
         this.port = port;
         server = new ServerFacade(port);
+        ws = new WebSocketFacade(port, this);
         preLogin = new PreLoginClient(server);
-        postLogin = new PostLoginClient(server);
-        gameplay = new GameplayClient(server);
+        postLogin = new PostLoginClient(server, ws);
+        gameplay = new GameplayClient(server, ws);
     }
 
     public void run() {
@@ -36,13 +47,19 @@ public class Repl {
                 if (state == State.PRE_LOGIN) {
                     result = preLogin.evaluate(line);
                     newState = preLogin.getNewState();
+                    authToken = preLogin.getAuthToken();
                 } else if (state == State.POST_LOGIN) {
+                    // should I assert we have an auth token?
+                    if (authToken == null) {
+                        throw new ResponseException(401, "Error, you do not have an auth token");
+                    }
                     currentGameID = null;
-                    result = postLogin.evaluate(line);
+                    result = postLogin.evaluate(line, authToken);
                     newState = postLogin.getNewState();
                     currentGameID = postLogin.getCurrentGameID();
+                    authToken = postLogin.getAuthToken();
                 } else {
-                    result = gameplay.evaluate(line, currentGameID);
+                    result = gameplay.evaluate(line, currentGameID, authToken);
                     newState = gameplay.getNewState();
                 }
                 System.out.println(result);
@@ -52,11 +69,12 @@ public class Repl {
             } catch (Exception ex) {
                 System.out.println("Error: " + ex.getMessage());
             }
-
-
         }
     }
 
+    public void notify(ServerMessage notification) {
+        System.out.println(SET_TEXT_COLOR_RED + notification.toString() + reset());
+    }
     public String printInput() { return SET_TEXT_COLOR_MAGENTA + ">>> "; }
 
     public String reset() { return RESET_TEXT_COLOR; }
