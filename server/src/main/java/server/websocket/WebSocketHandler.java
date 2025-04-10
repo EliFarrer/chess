8b64ba7593ea37a17d44b489439aac139a0d763f
Eli Farrer <eli.farrer@gmail.com>
2025-04-10 15:08:56 -0600

    added error handling and leave and makeMove

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -9,12 +9,12 @@ import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
-import server.ResponseException;
 import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
-
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -42,65 +42,69 @@ public class WebSocketHandler {
         }
         switch (command.getCommandType()) {
             case CONNECT -> connect(session, command.getAuthToken(), command.getGameID());
-            case MAKE_MOVE -> makeMove();
+            case MAKE_MOVE -> makeMove(command.getAuthToken(), command.getGameID());
             case LEAVE -> leave(command.getAuthToken());
-            case RESIGN -> resign();
+            case RESIGN -> resign(command.getAuthToken(), command.getGameID());
         }
     }
 
-    private void connect(Session session, String visitorAuthToken, Integer gameID) throws IOException, DataAccessException {
-        if (visitorAuthToken == null || visitorAuthToken.isEmpty()) {
-            ServerMessage message = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Not authenticated");
+    private void connect(Session session, String rootClientAuth, Integer gameID) throws IOException, DataAccessException {
+        if (rootClientAuth == null || rootClientAuth.isEmpty()) {
+            ErrorMessage message = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Not authenticated");
             session.getRemote().sendString(message.toString());
             return;
         }
-        // handle if they joined as a player or observer
-        String user = dataAccess.getUsername(visitorAuthToken);
+
+        String user;
+        try {
+            user = dataAccess.getUsername(rootClientAuth);
+        } catch (DataAccessException e) {
+            ErrorMessage message = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Not authenticated");
+            session.getRemote().sendString(message.toString());
+            return;
+        }
         GameData game = dataAccess.getGame(gameID);
         if (game == null) {
-            ServerMessage message = new ServerMessage(ServerMessage.ServerMessageType.ERROR, "Error: Bad gameID");
+            ErrorMessage message = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: Bad gameID");
             session.getRemote().sendString(message.toString());
             return;
         }
 
         // add a new connection to our connection manager
-        connections.add(session, visitorAuthToken);
+        connections.add(session, rootClientAuth);
+
+        // handle if they joined as a player or observer
         String status;
-        boolean isObserver = false;
         if (Objects.equals(user, game.whiteUsername())) {
             status = "white";
         } else if (Objects.equals(user, game.blackUsername())) {
             status = "black";
         } else {
             status = "an observer";
-            isObserver = true;
         }
 
+        var updateGame = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, "printed the game");
         var message = String.format("%s joined the game as %s", user, status);
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+        var notification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+        // broadcast the game to the user who just joined
+        connections.broadcastTo(rootClientAuth, updateGame);
         // notify the user joined for everyone currently in the game.
-        connections.broadcast(visitorAuthToken, notification);
-        var updateGame = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME, "printed the game");
-
-        // if it is an observer joining, the game will broadcast to them
-        // if it is someone joining the game, it will broadcast to everyone but them.
-        if (isObserver) {
-            connections.broadcastTo(visitorAuthToken, updateGame);
-        } else {
-            connections.broadcast(visitorAuthToken, updateGame);
-        }
-    }
-
-    private void makeMove() {
-
+        connections.broadcast(rootClientAuth, notification);
     }
 
     private void leave(String authToken) throws IOException, DataAccessException {
         connections.remove(authToken);
         String user = dataAccess.getUsername(authToken);
-        ServerMessage message = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, String.format("%s left the game", user));
+        NotificationMessage message = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, String.format("%s left the game", user));
         connections.broadcast(authToken, message);
     }
 
-    private void resign() {}
+    private void makeMove(String authToken, Integer gameID) throws IOException, DataAccessException {
+        LoadGameMessage newGameMessage = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, "new game");
+        NotificationMessage moveMessage = new NotificationMessage(ServerMessage.ServerMessageType.LOAD_GAME, "moved from here to there");
+        connections.broadcast(authToken, newGameMessage);
+        connections.broadcast(authToken, moveMessage);
+    }
+
+    private void resign(String authToken, Integer gameID) {}
 }
